7bd8fa3dc16e5a8f7269c2d4ad2529dff6783dc0
gonghao <18007926495@163.com>
2017-04-05 14:39:25 +0800

    categoryRight布局实现

--- app/src/main/java/com/shopping/app/jdmall/ui/fragment/CarFragment.java
+++ app/src/main/java/com/shopping/app/jdmall/ui/fragment/CarFragment.java
@@ -11,6 +11,11 @@ public class CarFragment extends BaseFragment {
 
 
     @Override
+    protected void init() {
+
+    }
+
+    @Override
     protected void startLoadData() {
 
     }
---
+++ app/src/main/java/com/shopping/app/jdmall/widget/CategoryItemView.java
@@ -0,0 +1,38 @@
+package com.shopping.app.jdmall.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.shopping.app.jdmall.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by 龚浩 on 2017/4/5.
+ */
+
+public class CategoryItemView extends LinearLayout {
+    @BindView(R.id.iv_icon)
+    ImageView mIvIcon;
+    @BindView(R.id.tv_name)
+    TextView mTvName;
+
+    public CategoryItemView(Context context) {
+        this(context, null);
+    }
+
+    public CategoryItemView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        View.inflate(getContext(), R.layout.view_category_item, this);
+        ButterKnife.bind(this);
+    }
+}

4016b89d97a230f48ec57f7def80b80a749d3de7
wanz <123456>
2017-04-06 11:22:31 +0800

    首页UI

--- app/src/main/java/com/shopping/app/jdmall/network/Api.java
+++ app/src/main/java/com/shopping/app/jdmall/network/Api.java
@@ -1,11 +1,19 @@
 package com.shopping.app.jdmall.network;
 
 
+import com.shopping.app.jdmall.bean.HomeTopicbean;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 /**
  * Retrofit的api接口,定义call方法
  *
  */
 
 public interface Api {
+    @GET("home")
+    Call<HomeTopicbean> listHome();
 
+    Call<HomeTopicbean.HomeTopicBean> listHome(int size);
 }
--- app/src/main/java/com/shopping/app/jdmall/ui/fragment/HomeFragment.java
+++ app/src/main/java/com/shopping/app/jdmall/ui/fragment/HomeFragment.java
@@ -1,37 +1,72 @@
 package com.shopping.app.jdmall.ui.fragment;
 
 
-import android.os.SystemClock;
+import android.util.Log;
 import android.view.View;
+import android.widget.BaseAdapter;
+
+import com.leon.loopviewpagerlib.FunBanner;
+import com.shopping.app.jdmall.app.Constant;
+import com.shopping.app.jdmall.bean.HomeTopicbean;
+import com.shopping.app.jdmall.network.HeiMaRetrofit;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 /**
  * 主页fragment
  */
-public class HomeFragment extends BaseFragment {
+public class HomeFragment extends BaseShopListFragment {
+
+    private String mPicture;
+    private static final String TAG = "HomeFragment";
+
+    @Override
+    protected View onCreateHeader() {
+        Log.d(TAG, "onCreateHeader: =====zoule");
+        return new FunBanner.Builder(getContext())
+                .setHeightWidthRatio(0.377f)
+                .setEnableAutoLoop(true)
+                .setImageUrlHost(Constant.HOST+"home/")
+                .setImageUrls(new String[]{mPicture})
+                .build();
+     
+
+    }
 
 
     @Override
     protected void startLoadData() {
-        //测试
-        new Thread(new Runnable() {
+        Call<HomeTopicbean> homeBeanCall = HeiMaRetrofit.getInstance().getApi().listHome();
+        homeBeanCall.enqueue(new Callback<HomeTopicbean>() {
             @Override
-            public void run() {
-                SystemClock.sleep(2000);
-                getActivity().runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
+            public void onResponse(Call<HomeTopicbean> call, Response<HomeTopicbean> response) {
+                //将应用列表数据加入数据集合中
+                Log.d(TAG, "onresponse: =="+ response.body());
 
-                        onDataLoadedFailed();
-                    }
-                });
-
+                //保存轮播图数据
+                mPicture = response.body().getHomeTopic().get(getId()).getPic();
+                onDataLoadedSuccess();
             }
-        }).start();
+            @Override
+            public void onFailure(Call<HomeTopicbean> call, Throwable t) {
+                Log.d(TAG, "onFailure: =="+t.getLocalizedMessage());
+                onDataLoadedFailed();
+            }
+        });
+
+
     }
 
+
     @Override
-    protected View onCreateContentView() {
+    protected void onStartLoadMore() {
+
+    }
+    @Override
+    protected BaseAdapter oncreateAdapter() {
         return null;
     }
 }

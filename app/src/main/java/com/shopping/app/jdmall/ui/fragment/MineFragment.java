3da8f63e496e603b8061f2c15f3fefd8fca77870
gz <104019@qq.com>
2017-04-06 09:14:31 +0800

    布局显示

--- app/src/main/java/com/shopping/app/jdmall/ui/fragment/MineFragment.java
+++ app/src/main/java/com/shopping/app/jdmall/ui/fragment/MineFragment.java
@@ -1,27 +1,16 @@
 package com.shopping.app.jdmall.ui.fragment;
 
 
-import android.view.View;
-
+import com.shopping.app.jdmall.R;
 
 /**
  * 我的fragment
  */
-public class MineFragment extends BaseFragment {
+public class MineFragment extends BaseNotLoadDataFragment {
 
 
     @Override
-    protected void init() {
-
-    }
-
-    @Override
-    protected void startLoadData() {
-
-    }
-
-    @Override
-    protected View onCreateContentView() {
-        return null;
+    public int getResId() {
+        return R.layout.mine_fragment;
     }
 }

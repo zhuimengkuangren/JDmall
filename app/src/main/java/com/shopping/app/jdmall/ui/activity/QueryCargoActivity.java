ddfcdfad7468de9167f100a1dc6dccd01c5fc9e3
gonghao <18007926495@163.com>
2017-04-06 11:00:42 +0800

    实现点击item跳转到查询商品界面

---
+++ app/src/main/java/com/shopping/app/jdmall/ui/activity/QueryCargoActivity.java
@@ -0,0 +1,13 @@
+package com.shopping.app.jdmall.ui.activity;
+
+import com.shopping.app.jdmall.R;
+
+/**
+ * Created by 龚浩 on 2017/4/6.
+ */
+public class QueryCargoActivity extends BaseActivity{
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_query_cargo;
+    }
+}

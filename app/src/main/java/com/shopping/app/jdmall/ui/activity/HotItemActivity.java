edc93e25c24ec723a7b40840abe23afaaa8a9855
Your Name <you@example.com>
2017-04-06 15:23:06 +0800

    添加了促销,新品热门,推荐四个Activity

--- app/src/main/java/com/shopping/app/jdmall/ui/activity/HotItemActivity.java
+++ app/src/main/java/com/shopping/app/jdmall/ui/activity/HotItemActivity.java
@@ -1,8 +1,24 @@
 package com.shopping.app.jdmall.ui.activity;
 
+import android.widget.BaseAdapter;
+
 /**
  * Created by Administrator on 2017/4/6.
  */
 
 public class HotItemActivity extends BaseListLoadMoreActivity {
+    @Override
+    protected void startLoadMoreData() {
+
+    }
+
+    @Override
+    protected BaseAdapter oncreateAdapter() {
+        return null;
+    }
+
+    @Override
+    protected void startLoadData() {
+
+    }
 }

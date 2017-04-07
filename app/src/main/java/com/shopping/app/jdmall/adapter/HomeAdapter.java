44565362071621992462998f238026f53ff6963b
qipan <626928324@qq.com>
2017-04-07 14:56:06 +0800

    Merge branch 'developer' of http://192.168.30.30:8888/r/JDMall into developer
    # Conflicts:
    #	app/src/main/java/com/shopping/app/jdmall/network/Api.java

--- app/src/main/java/com/shopping/app/jdmall/adapter/HomeAdapter.java
+++ app/src/main/java/com/shopping/app/jdmall/adapter/HomeAdapter.java
@@ -7,7 +7,6 @@ import android.widget.TextView;
 import com.shopping.app.jdmall.bean.BannerBean;
 import com.shopping.app.jdmall.widget.PanBannerView;
 import com.shopping.app.jdmall.widget.PanCategroy;
-import com.shopping.app.jdmall.widget.PanImageView;
 import com.shopping.app.jdmall.widget.PanMarqueeView;
 import com.shopping.app.jdmall.widget.RecommendScrollView;
 import com.shopping.app.jdmall.widget.SecondKill;
@@ -20,15 +19,12 @@ import java.util.List;
 
 public class HomeAdapter extends BaseListLoadAdapter {
 
-    private static final int ITEM_TYPE_FIRST = 0;
-    private static final int ITEM_TYPE_CATEGROY = 1;
-    private static final int ITEM_TYPE_MARQUEE = 2;
-    private static final int ITEM_TYPE_SCOENDKILL = 3;
-    private static final int ITEM_TYPE_IMAGE01 = 4;
-    private static final int ITEM_TYPE_RecommendScrollView = 5;
-    private static final int ITEM_TYPE_IMAGE02 = 6;
-    private static final int ITEM_TYPE_BANNER02 = 7;
-    private static final int ITEM_TYPE_NORMAL = 9;
+    private static final int ITEM_TYPE_CATEGROY = 0;
+    private static final int ITEM_TYPE_MARQUEE = 1;
+    private static final int ITEM_TYPE_SCOENDKILL = 2;
+    private static final int ITEM_TYPE_RecommendScrollView = 3;
+    private static final int ITEM_TYPE_BANNER02 = 4;
+    private static final int ITEM_TYPE_NORMAL = 5;
     private BannerBean mBean;
     private Context mContext;
     private List mList;
@@ -54,27 +50,20 @@ public class HomeAdapter extends BaseListLoadAdapter {
 
     @Override
     public int getViewTypeCount() {
-        return 10;
+        return 6;
     }
 
     @Override
     public int getItemViewType(int position) {
-
         if (position == 0) {
-            return ITEM_TYPE_FIRST;
-        } else if (position == 1) {
             return ITEM_TYPE_CATEGROY;
-        } else if (position == 2) {
+        } else if (position == 1) {
             return ITEM_TYPE_MARQUEE;
-        } else if (position == 3) {
+        } else if (position == 2) {
             return ITEM_TYPE_SCOENDKILL;
-        } else if (position == 4) {
-            return ITEM_TYPE_IMAGE01;
-        } else if (position == 5) {
+        } else if (position == 3) {
             return ITEM_TYPE_RecommendScrollView;
-        } else if (position == 6) {
-            return ITEM_TYPE_IMAGE02;
-        } else if (position == 7) {
+        } else if (position == 4) {
             return ITEM_TYPE_BANNER02;
         }
         return ITEM_TYPE_NORMAL;
@@ -82,23 +71,14 @@ public class HomeAdapter extends BaseListLoadAdapter {
 
     @Override
     protected View onCreateView(int position) {
-
-        if (getItemViewType(position) == ITEM_TYPE_FIRST) {
-            return new TextView(getContext());
-        } else if (getItemViewType(position) == ITEM_TYPE_CATEGROY) {
+        if (getItemViewType(position) == ITEM_TYPE_CATEGROY) {
             return new PanCategroy(getContext());
         } else if (getItemViewType(position) == ITEM_TYPE_MARQUEE) {
             return new PanMarqueeView(getContext());
         } else if (getItemViewType(position) == ITEM_TYPE_SCOENDKILL) {
             return new SecondKill(getContext());
-        } else if (getItemViewType(position) == ITEM_TYPE_IMAGE01) {
-            return new PanImageView(getContext());
         } else if (getItemViewType(position) == ITEM_TYPE_RecommendScrollView) {
             return new RecommendScrollView(getContext());
-        } else if (getItemViewType(position) == ITEM_TYPE_IMAGE02) {
-            TextView textView = new TextView(getContext());
-            //textView.setText("哈哈");
-            return textView;
         } else if (getItemViewType(position) == ITEM_TYPE_BANNER02) {
             return new PanBannerView(getContext());
         }

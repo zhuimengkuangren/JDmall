1c5717daf682df10f185f7bb26a2306b306621d7
Your Name <you@example.com>
2017-04-07 15:26:33 +0800

    完成limitbuy的网络请求,正在做新品上架

--- app/src/main/java/com/shopping/app/jdmall/widget/NewProductView.java
+++ app/src/main/java/com/shopping/app/jdmall/widget/NewProductView.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 
 import com.shopping.app.jdmall.R;
+import com.shopping.app.jdmall.bean.FindBean;
 
 /**
  * Created by Administrator on 2017/4/6.
@@ -23,5 +24,9 @@ public class NewProductView extends LinearLayout {
 
     private void initData() {
         LayoutInflater.from(getContext()).inflate(R.layout.view_new_product,this);
+    }
+
+    public void bindView(FindBean.ProductListBean productListBean) {
+
     }
 }

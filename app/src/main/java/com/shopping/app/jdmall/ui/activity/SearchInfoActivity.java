ff2538bdbd7d17c0615a739bb78be2f0618a9ac7
qipan <626928324@qq.com>
2017-04-08 10:08:00 +0800

    添加首页限时抢购点击跳转

--- app/src/main/java/com/shopping/app/jdmall/ui/activity/SearchInfoActivity.java
+++ app/src/main/java/com/shopping/app/jdmall/ui/activity/SearchInfoActivity.java
@@ -2,22 +2,26 @@ package com.shopping.app.jdmall.ui.activity;
 
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.shopping.app.jdmall.R;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by 龚浩 on 2017/4/6.
  */
 
-public class SearchActivity extends BaseActivity {
+public class SearchInfoActivity extends BaseActivity {
     @BindView(R.id.iv_back)
     ImageView mIvBack;
     @BindView(R.id.iv_voice)
     ImageView mIvVoice;
     @BindView(R.id.et_search)
     EditText mEtSearch;
+    @BindView(R.id.list_search)
+    ListView mListSearch;
 
     @Override
     protected int getLayoutResId() {
@@ -26,9 +30,11 @@ public class SearchActivity extends BaseActivity {
 
     @Override
     protected void init() {
-        super.init();
+        ButterKnife.bind(this, this);
+        startLoadDatas();   //从网上请求数据
     }
 
+    private void startLoadDatas() {
 
-
+    }
 }

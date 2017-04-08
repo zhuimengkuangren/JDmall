bf2954263777853297bfb14b1ed20cc5d1210bde
Lau-Daniel <2067168016@qq.com>
2017-04-08 11:42:26 +0800

    创建sku转换工具类

--- app/src/main/java/com/shopping/app/jdmall/utils/SkuTransferUtils.java
+++ app/src/main/java/com/shopping/app/jdmall/utils/SkuTransferUtils.java
@@ -2,6 +2,7 @@ package com.shopping.app.jdmall.utils;
 
 import com.shopping.app.jdmall.bean.SkuBean;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,7 +11,7 @@ import java.util.List;
 
 public class SkuTransferUtils {
 
-    public static String BeanToString(List<SkuBean> skuList){
+    public static String ListToString(List<SkuBean> skuList) {
         String result = "";
         for (int i = 0; i < skuList.size(); i++) {
             SkuBean skuBean = skuList.get(i);
@@ -19,7 +20,35 @@ public class SkuTransferUtils {
             int[] attrs = skuBean.getAttrs();
             result += id + ":" + num + ":" + attrs[0] + "," + attrs[1] + "|";
         }
-        return result;
+        return result.substring(0, result.length() - 1);
     }
+
+    public static List<SkuBean> StringToList(String str) {
+        ArrayList<SkuBean> skuList = new ArrayList<>();
+
+        String replaceStr = str.replace("|", "#");//split无法识别"|",这是一个bug?
+        String[] beanStrs = replaceStr.split("#");//bean字符串数组
+        for (int i = 0; i < beanStrs.length; i++) {
+            String beanStr = beanStrs[i];//bean字符串
+            String[] beanUnits = beanStr.split(":");//bean元素数组
+            int id = Integer.parseInt(beanUnits[0]);//id
+            int num = Integer.parseInt(beanUnits[1]);//数量
+            String[] beanattrUnits = beanStr.split(",");//属性元素数组
+            int color = Integer.parseInt(beanUnits[0]);//颜色
+            int size = Integer.parseInt(beanUnits[1]);//尺码
+            int[] attrs = new int[]{color, size};
+
+            //填充数据
+            SkuBean skuBean = new SkuBean();
+            skuBean.setNum(num);
+            skuBean.setId(id);
+            skuBean.setAttrs(attrs);
+
+            skuList.add(skuBean);
+        }
+
+        return skuList;
+    }
+
 
 }

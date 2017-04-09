ee702ca4926168a9e932ca06be6a40c290ad05c8
gonghao <18007926495@163.com>
2017-04-09 20:45:51 +0800

    实现录音功能

--- app/src/main/java/com/shopping/app/jdmall/ui/fragment/HomeFragment.java
+++ app/src/main/java/com/shopping/app/jdmall/ui/fragment/HomeFragment.java
@@ -1,9 +1,14 @@
 package com.shopping.app.jdmall.ui.fragment;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.shopping.app.jdmall.bean.CategoryItemBean;
 import com.shopping.app.jdmall.network.JDRetrofit;
@@ -25,6 +30,37 @@ public class HomeFragment extends BaseFragment {
 
     private CategoryItemBean mCategoryItemBean;
     private HomeSearchView mHomeSearchView;
+
+    @Override
+    protected void init() {
+        super.init();
+        //申请权限
+        initPermission();
+    }
+
+    private void initPermission() {
+
+        int result = ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.RECORD_AUDIO);
+        if(result == PackageManager.PERMISSION_DENIED) {
+          /*  Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            Uri uri = Uri.fromParts("package", getPackageName(), null);
+            intent.setData(uri);
+            startActivity(intent);
+*/
+            Toast.makeText(getContext(),"向用户请求权限",Toast.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(getActivity(),new String[] {Manifest.permission.RECORD_AUDIO},0);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        if(requestCode == 0) {
+            if(grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                Toast.makeText(getContext(),"用户拒绝了权限请求",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     @Override
     protected void startLoadData() {
